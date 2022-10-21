a3e46e6bcf35840ac14ec9a01d5fc359022a36a5
Your NameBartosz <bberezowski7@gmail.com>
2022-10-21 17:50:19 +0200

    First commit

---
+++ build/CMakeFiles/My_chess_Game.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/My_chess_Game.dir/main.cpp.o"
+  "CMakeFiles/My_chess_Game.dir/main.cpp.o.d"
+  "My_chess_Game"
+  "My_chess_Game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/My_chess_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
